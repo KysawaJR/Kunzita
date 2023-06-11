4aa0da57d96ca1245abea1326bcad254c6dabdce
Kysawa Junior <115995661+KysawaJR@users.noreply.github.com>
2023-06-10 21:44:08 -0300

    Update App.kt

--- src/kotlin/App/App.kt
+++ src/kotlin/App/App.kt
@@ -1,4 +1,4 @@
-// Import necessary libraries
+
 import kotlin.coroutines.*
 import androidx.compose.foundation.*
 import androidx.compose.material.*
@@ -7,78 +7,92 @@ import androidx.compose.ui.*
 import androidx.compose.ui.layout.*
 import androidx.compose.ui.unit.*
 
-// Define the framework
-object MultiplatformAppFramework {
-  // Define necessary variables and constants
-  private var appRunning = false
-  private val platforms = mutableListOf<Platform>()
-  
-  // Define the platform interface
-  interface Platform {
-    val name: String
-    suspend fun initialize()
-    suspend fun run(app: @Composable () -> Unit)
-  }
-  
-  // Define a function to add a platform
-  fun addPlatform(platform: Platform) {
-    platforms.add(platform)
-  }
-  
-  // Define a function to start the app on all platforms
-  fun startApp(app: @Composable () -> Unit) {
-    if (!appRunning) {
-      appRunning = true
-      platforms.forEach { platform ->
-        launch {
-          platform.initialize()
-          platform.run(app)
-        }
-      }
-    }
+@Composable
+fun DesktopApp(
+  title : String,
+  size : IntSize,
+  UI: @Composable () -> Unit,
+  dataframe: Unit
+){
+  Window(
+    title = title,
+    size = size
+  ){
+    UI()
   }
 }
 
-// Define a desktop platform
-class DesktopPlatform : MultiplatformAppFramework.Platform {
-  override val name: String = "Desktop"
-  
-  override suspend fun initialize() {
-    // Initialize desktop platform
-  }
-  
-  override suspend fun run(app: @Composable () -> Unit) {
-    // Run app on desktop platform
-    Window(
-      title = "Multiplatform App",
-      size = IntSize(800, 600)
-    ) {
-      MaterialTheme {
-        app()
-      }
-    }
-  }
-}
 
-// Define an Android platform
-class AndroidPlatform : MultiplatformAppFramework.Platform {
-  override val name: String = "Android"
-  
-  override suspend fun initialize() {
-    // Initialize Android platform
-  }
-  
-  override suspend fun run(app: @Composable () -> Unit) {
-    // Run app on Android platform
-    TODO()
-  }
-}
 
-// Add platforms to the framework
-MultiplatformAppFramework.addPlatform(DesktopPlatform())
-MultiplatformAppFramework.addPlatform(AndroidPlatform())
 
-// Define the app
+
+  
+
+
+
+
+
+
+  
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
 fun main() = MultiplatformAppFramework.startApp {
   // Define app here using Jetpack Compose and Kotlin
   Text("Hello, World!")

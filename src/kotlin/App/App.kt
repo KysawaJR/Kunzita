4f0c48dab92a3cfede169fd26ab075ea52836470
Kysawa Junior <115995661+KysawaJR@users.noreply.github.com>
2023-06-18 14:46:35 -0300

    Update App.kt

--- src/kotlin/App/App.kt
+++ src/kotlin/App/App.kt
@@ -7,34 +7,21 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.layout.*
 import androidx.compose.ui.unit.*
+import app.*
 
-@Composable
-fun DesktopApp(
-  title : String,
-  size : IntSize,
-  ui: @Composable () -> Unit,
-  dataframe: Unit? = null
-){
-  Window(
-    title = title,
-    size = size
-  ){
-    ui()
+object Desktop {
+
+  fun App(val title: String) {
+    Window(
+      onExitRequest = ::exitApplication(),
+      title = title,
+      state = WindowState(size = DpSize(1000.dp, 600.dp), placement = WindowPlacement.Center)
+    ){
+      UI.render()
+    }
+  }
+
+  fun exitApplication() {
+    exitProcess(0)
   }
 }
-
-fun main() = DesktopApp(
-  title = "Desktop App",
-  size = IntSize(1000.dp, 600.dp)
-){
-  UI()
-}
-
-@Composable
-fun UI() {
-  Box(
-    Modifier.fillMaxSize(),
-    color = Color.White
-  )
-}
-
